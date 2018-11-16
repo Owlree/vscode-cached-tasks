280cd13aa20b2bf47f2a144351fd3c5beb5d1f9a
Robert Badea <owlree128@gmail.com>
2018-11-16 15:13:05 +0200

    Added extension logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,38 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from 'vscode';
+import { isString } from 'util';
+
+
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "cached-tasks" is now active!');
+    let tasksDictionary: { [name: string] : vscode.Task } = {};
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let RunTask = vscode.commands.registerCommand('extension.RunTask', () => {
+        let keys = Object.keys(tasksDictionary);
+        keys.sort();
+        vscode.window.showQuickPick(keys).then(choice => {
+            if (isString(choice)) {
+                vscode.window.showInformationMessage(`Cached Tasks: Running ${choice}`);
+                vscode.tasks.executeTask(tasksDictionary[choice]);
+            }
+        });
     });
 
-    context.subscriptions.push(disposable);
+    let ScanTasks = vscode.commands.registerCommand('extension.ScanTasks', () => {
+        vscode.window.showInformationMessage('Cached Tasks: Scanning tasks');
+        vscode.tasks.fetchTasks().then(tasks => {
+            vscode.window.showInformationMessage('Cached Tasks: Scanning tasks done');
+            for (let task of tasks) {
+                tasksDictionary[task.name] = task;
+            }
+        });
+    });
+
+    context.subscriptions.push(...[RunTask, ScanTasks]);
+    vscode.commands.executeCommand('extension.ScanTasks');
 }
 
-// this method is called when your extension is deactivated
+
 export function deactivate() {
 }
